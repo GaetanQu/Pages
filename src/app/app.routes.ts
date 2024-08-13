0273bef390bb554063a9e5f867075cf0b28c9972
GaetanQu <gaetan.quenouille.gq@gmail.com>
2024-08-13 13:40:36 +0200

    ✨ feat(Projects): Added projects page
    Added a new page comonent to hold the partials as the header and the projects & future contact pages and the associated routing. Added a photo to the landing page too.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'projects', component: ProjectsComponent }
+];
---
+++ src/app/pages/projects/projects.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../partials/header/header.component';
+
+@Component({
+  selector: 'app-projects',
+  standalone: true,
+  imports: [
+    HeaderComponent
+  ],
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.css'
+})
+export class ProjectsComponent {
+
+}
