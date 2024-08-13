021dbdb69af8eb8e2c855696baf6007f6fb997dc
GaetanQu <gaetan.quenouille.gq@gmail.com>
2024-08-13 14:51:52 +0200

    🦄 refactor(Routes):
    Refactored the routes for the pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,21 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { PagesComponent } from './pages/pages.component';
 import { ProjectsComponent } from './pages/projects/projects.component';
 
 export const routes: Routes = [
-    { path: '', component: LandingPageComponent },
-    { path: 'projects', component: ProjectsComponent }
+    { 
+        path: '',
+        component: LandingPageComponent
+    },
+    {
+        path: 'portfolio',
+        component: PagesComponent,
+        children: [
+            {
+                path: "projects",
+                component: ProjectsComponent
+            }
+        ]
+    }
 ];
--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../partials/header/header.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [],
+  imports: [
+    HeaderComponent,
+    RouterOutlet
+  ],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.css'
 })
